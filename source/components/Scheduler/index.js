c59070f8e017a7a6268c28a674af51d777b884f7
Dmytro Nazarenko <dmamyk@gmail.com>
2019-12-01 11:15:55 +0200

    RT1: + implement router

---
+++ source/components/Scheduler/index.js
@@ -0,0 +1,13 @@
+// Core
+import React, { Component } from 'react';
+
+// Styles
+import Styles from './styles.m.css';
+
+export default class Scheduler extends Component {
+    render() {
+        return (
+            <h1>scheduler</h1>
+        )
+    }
+}
